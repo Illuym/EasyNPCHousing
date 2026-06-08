998d82f2da281c326c7de117c8545a29663df6c3
Illuym <60437152+Illuym@users.noreply.github.com>
2026-06-08 19:24:02 -0400

    Adding Multiplayer Support (#3)
    Houses were not syncing correctly on multiplayer worlds, they now are

--- Content/EasyNPCHousing.cs
+++ Content/EasyNPCHousing.cs
@@ -1,3 +1,6 @@
+using System.IO;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace EasyNPCHousing.Content
@@ -5,6 +8,19 @@ namespace EasyNPCHousing.Content
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class EasyNPCHousing : Mod
 	{
+        /// <summary>
+        /// Create the house on the server and update the area for all clients
+        /// </summary>
+        public override void HandlePacket(BinaryReader reader, int whoAmI)
+        {
+            int x = reader.ReadInt32();
+            int y = reader.ReadInt32();
 
-	}
+            if (Main.netMode == NetmodeID.Server)
+            {
+                EasyNPCHousingBuilder.BuildNPCHouse(x, y);
+                NetMessage.SendTileSquare(-1, x, y, 100);
+            }
+        }
+    }
 }
